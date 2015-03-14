5ee7d1101503fbe5d26ba65a0711319ed796a3c7
jaden <jaden@HAL-9000>
2015-03-13 22:39:27 -0500

    I hope to hell this works. It's ugly as shit.

--- src/hom03/LinkedStack.java
+++ src/hom03/LinkedStack.java
@@ -8,18 +8,17 @@ package hom03;
 public class LinkedStack<T> {
 
 	private LinkedBag<T> list = new LinkedBag<>();
-	private int size = 0;
 
 	public LinkedStack() {
 		//do nothing
 	}
 
 	public int getSize() {
-		return size;
+		return list.getCurrentSize();
 	}
 
 	public boolean isEmpty() {
-		return size == 0;
+		return list.getCurrentSize() == 0;
 	}
 
 	public void push(T element) {
